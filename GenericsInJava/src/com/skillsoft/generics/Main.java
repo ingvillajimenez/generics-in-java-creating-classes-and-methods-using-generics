107d2faffd346af33e770a2a3f942d678a466421
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-25 14:05:39 -0600

    Parameterizing Classes to Call Parameterized Methods

--- GenericsInJava/src/com/skillsoft/generics/Main.java
+++ GenericsInJava/src/com/skillsoft/generics/Main.java
@@ -6,39 +6,27 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
 
-        Map<Integer, String> studentsMap = new HashMap<>();
+        Map<Integer, String> studentNamesMap = new HashMap<>();
 
         Integer[] studentIds = new Integer[] {22, 33, 44, 55};
         String[] studentNames = new String[] {"Doris", "Donald", "Elise", "Eric"};
 
-        MapHelper mapHelper = new MapHelper();
+        MapHelper<Integer, String> mapHelper1 = new MapHelper<>();
 
-        mapHelper.addEntries(studentsMap, studentIds, studentNames);
+        mapHelper1.addEntries(studentNamesMap, studentIds, studentNames);
 
-        mapHelper.printEntries(studentsMap);
+        mapHelper1.printEntries(studentNamesMap);
 
-//        Map<Integer, Double> studentsMap = new HashMap<>();
-//
-//        Integer[] studentIds = new Integer[] {22, 33, 44, 55};
-//        Double[] studentScores = new Double[] {3.2, 2.8, 3.3, 3.8};
-//
-//        MapHelper mapHelper = new MapHelper();
-//
-//        mapHelper.addEntries(studentsMap, studentIds, studentScores); // java: incompatible types: java.util.Map<java.lang.Integer,java.lang.Double> cannot be converted to java.util.Map<java.lang.Integer,java.lang.String>
-//
-//        System.out.println(studentsMap);
 
-//    Map<Integer, String> studentsMap = new HashMap<>();
-//
-//    Integer[] studentIds = new Integer[] {22, 33, 44, 55};
-//    String[] studentNames = new String[] {"Doris", "Donald", "Elise", "Eric"};
-//
-//    MapHelper mapHelper = new MapHelper();
-//
-//    mapHelper.addEntries(studentsMap, studentIds, studentNames);
-//
-//    System.out.println(studentsMap);
+        Map<Integer, Double> studentScoresMap = new HashMap<>();
 
+        Double[] studentScores = new Double[] {3.2, 2.8, 3.3, 3.8};
+
+        MapHelper<Integer, Double> mapHelper2 = new MapHelper<>();
+
+        mapHelper2.addEntries(studentScoresMap, studentIds, studentScores);
+
+        mapHelper2.printEntries(studentScoresMap);
     }
 }
 

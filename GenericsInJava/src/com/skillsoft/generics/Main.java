6c574d63944833a21c45b682b57bafbd96532cd5
Juan Villa <ing.villa.jimenez@gmail.com>
2023-07-25 15:52:50 -0600

    Identifying Type Inference for Parameterized Methods

--- GenericsInJava/src/com/skillsoft/generics/Main.java
+++ GenericsInJava/src/com/skillsoft/generics/Main.java
@@ -1,29 +1,58 @@
 package com.skillsoft.generics;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
 
-        Map<Integer, String> studentNamesMap = new HashMap<>();
+        Map<Integer, String> map1 = new HashMap<>();
+        map1.put(1, "Alice");
+        map1.put(2, "Bob");
+        map1.put(3, "Charlie");
 
-        Integer[] studentIds = new Integer[] {22, 33, 44, 55};
-        String[] studentNames = new String[] {"Doris", "Donald", "Elise", "Eric"};
+        Map<Double, String> map2 = new HashMap<>();
+        map2.put(3.1, "Charles");
+        map2.put(2.3, "Bob");
+        map2.put(1.4, "Alice");
 
-//        MapHelper.addEntries(studentNamesMap, studentIds, studentNames);
-        MapHelper.<Integer, String>addEntries(studentNamesMap, studentIds, studentNames); // Explicit type arguments can be inferred
+//        Map<Integer, String> map2 = new HashMap<>();
+////        map2.put(3, "Charlie");
+//        map2.put(3, "Charles");
+//        map2.put(2, "Bob");
+//        map2.put(1, "Alice");
 
-//        MapHelper.printEntries(studentNamesMap);
-        MapHelper.<Integer, String>printEntries(studentNamesMap); // Explicit type arguments can be inferred
+//        Map<Integer, Double> map2 = new HashMap<>();
+//        map2.put(3, 5.5);
+//        map2.put(2, 4.5);
+//        map2.put(1, 8.5);
 
-        Map<Integer, Double> studentScoresMap = new HashMap<>();
+        System.out.println("Are the maps equal? " + MapHelper.mapEquals(map1, map2));
+//        java: method mapEquals in class com.skillsoft.generics.MapHelper cannot be applied to given types;
+//        required: java.util.Map<K,V>,java.util.Map<K,V>
+//        found:    java.util.Map<java.lang.Integer,java.lang.String>,java.util.Map<java.lang.Double,java.lang.String>
+//        reason: inference variable K has incompatible equality constraints java.lang.Double,java.lang.Integer
 
-        Double[] studentScores = new Double[] {3.2, 2.8, 3.3, 3.8};
 
-        MapHelper.<Integer, Double>addEntries(studentScoresMap, studentIds, studentScores); // Explicit type arguments can be inferred
+//        MapHelper.displayElements(100, 200, 300, 400);
+//
+//        MapHelper.displayElements("Key", "Value", new Date(), new UUID(543, 98765));
+//
+//        MapHelper.displayElements(new Date(), "Hello", 123.4);
 
-        MapHelper.<Integer, Double>printEntries(studentScoresMap); // Explicit type arguments can be inferred
+//        MapHelper.displayElements("ID", 10);
+//
+//        MapHelper.displayElements(10001, new Date());
+//
+//        MapHelper.displayElements(1045.56, new UUID(123, 567));
+
+//        MapHelper.displayElements("Key", "Value");
+//
+//        MapHelper.displayElements(new Date(), new Date());
+//
+//        MapHelper.displayElements(1045.56, 23456.12);
     }
 }
 
--- GenericsInJava/src/com/skillsoft/generics/MapHelper.java
+++ GenericsInJava/src/com/skillsoft/generics/MapHelper.java
@@ -2,11 +2,10 @@ package com.skillsoft.generics;
 
 import java.util.Map;
 
-//public class MapHelper<K, V> {
-public class MapHelper { // MapHelper does not need to be a generic class if the static methods are parameterized
+public class MapHelper {
 
     public static <K, V> void addEntries(Map<K, V> map, K[] keys, V[] values) {
-    // Specify the type parameters within angular brackets after the static keyword and before the return type
+
         int index = 0;
         for (K key : keys) {
             map.put(key, values[index]);
@@ -15,7 +14,6 @@ public class MapHelper { // MapHelper does not need to be a generic class if the
     }
 
     public static <K,V> void printEntries(Map<K, V> map) {
-        // you need to specify the parameterized types for each method individually (generic method!)
         System.out.println("\n***********Map entries");
 
         for (Map.Entry<K, V> entry : map.entrySet()) {
@@ -23,21 +21,43 @@ public class MapHelper { // MapHelper does not need to be a generic class if the
         }
     }
 
-//    public static void addEntries(Map<K, V> map, K[] keys, V[] values) { // 'com.skillsoft.generics.MapHelper.this' cannot be referenced from a static context
-//    // You cannot use generic types for static variables, you cannot use the parameterized type in a static context
-//        int index = 0;
-//        for (K key : keys) {
-//            map.put(key, values[index]);
-//            index++;
-//        }
-//    }
+//    public static <T> void displayElements(T element1, T element2) {
+//        // This is a generic method because of using the type parameter <T>
+//        System.out.println("\n************Some elements");
 //
-//    public static void printEntries(Map<K, V> map) {
-//        System.out.println("\n***********Map entries");
-//
-//        for (Map.Entry<K, V> entry : map.entrySet()) {
-//            System.out.println(entry);
-//        }
+//        System.out.format("Element 1: %s, Element 2: %s\n", element1, element2);
 //    }
+
+    public static <T, S> void displayElements(T element1, S element2) {
+        System.out.println("\n*************Some elements");
+
+        System.out.format("Element 1: %s, Element 2: %s\n", element1, element2);
+    }
+
+    @SafeVarargs // to prevent Possible heap pollution from parameterized vararg type
+    public static <T> void displayElements(T... elements) { // varargs type
+        System.out.println("\n***********Some elements");
+
+        for (T element : elements) {
+            System.out.println(element);
+        }
+    }
+
+    public static <K, V> boolean mapEquals(Map<K, V> map1, Map<K, V> map2) {
+
+        if (map1.entrySet().size() != map2.entrySet().size()) {
+            return false;
+        }
+
+        for (Map.Entry<K, V> entry : map1.entrySet()) {
+            V map2Value = map2.get(entry.getKey());
+
+            if (!entry.getValue().equals(map2Value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
 
 }
